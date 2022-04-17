24a950b43180ca46d1d260d6aa9d0a84f378f5f7
ShamsiBakirov <bekirov.shemsi@mail.ru>
2022-04-17 11:54:39 +0400

    changed

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        boolean bool=true;
         System.out.println("aaa");
+        if(bool){
+            System.out.println(bool);
+        }
     }
 }
