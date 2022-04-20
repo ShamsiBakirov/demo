be4db6ba2ad79db0afe6afe80d6d3a566301c5ce
ShamsiBakirov <bekirov.shemsi@mail.ru>
2022-04-21 01:59:04 +0400

    changed

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("BRANCH-0.1");
+        System.out.println("Changed");
     }
 }
